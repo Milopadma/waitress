4a63ab1325f6832042cccabd00da4233c30f96bc
Milopadma <Milopadma@yahoo.com>
2022-10-09 17:07:01 +0800

    command is now slash grou[

--- src/commands/atme.ts
+++ src/commands/atme.ts
@@ -1,14 +1,19 @@
 import { ApplicationCommandOptionType, CommandInteraction } from "discord.js";
-import { Discord, Slash, SlashChoice, SlashOption } from "discordx";
+import { Discord, Slash, SlashChoice, SlashGroup, SlashOption } from "discordx";
 import type { User } from "discord.js";
 
 //define listeners array
 const atMeListenersPair: [User, User][] = [];
 
 @Discord()
+@SlashGroup({
+  description: "notifies you when someone joins a vc",
+  name: "atme",
+})
 export class AtMe {
-  @Slash({ description: "notifies you when someone joins a vc", name: "atme" })
-  async atme(
+  @Slash({ description: "add a listener" })
+  @SlashGroup("atme")
+  async add(
     @SlashOption({
       name: "user",
       description:
@@ -76,6 +81,31 @@ export class AtMe {
           }
         });
       }
+    }
+  }
+  @Slash({ description: "remove a listener" })
+  @SlashGroup("atme")
+  async remove(
+    @SlashOption({
+      name: "user",
+      description:
+        "Who do you want to be notified for when they join a voice channel?",
+      required: true,
+      type: ApplicationCommandOptionType.User,
+    })
+    user: User | undefined,
+    interaction: CommandInteraction
+  ): Promise<void> {
+    //check if the user pair is already in the atMeListeners array
+    const userPair = atMeListenersPair.find((pair) => pair[0] === user);
+    if (userPair) {
+      //if it is, remove it
+      atMeListenersPair.splice(atMeListenersPair.indexOf(userPair), 1);
+      //reply for confirmation
+      await interaction.reply(`No longer notifying ${user}`);
+    } else {
+      //if it isn't, reply for confirmation
+      await interaction.reply(`Not notifying ${user}`);
     }
   }
 }

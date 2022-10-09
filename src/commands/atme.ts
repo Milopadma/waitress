49d8aba87a141bd77b5d23514d91d4617bbd28db
Milopadma <Milopadma@yahoo.com>
2022-10-09 16:59:36 +0800

    somewhat works

--- src/commands/atme.ts
+++ src/commands/atme.ts
@@ -1,5 +1,5 @@
 import { ApplicationCommandOptionType, CommandInteraction } from "discord.js";
-import { Discord, Slash, SlashOption } from "discordx";
+import { Discord, Slash, SlashChoice, SlashOption } from "discordx";
 import type { User } from "discord.js";
 
 //define listeners array
@@ -10,13 +10,22 @@ export class AtMe {
   @Slash({ description: "notifies you when someone joins a vc", name: "atme" })
   async atme(
     @SlashOption({
+      name: "user",
       description:
         "Who do you want to be notified for when they join a voice channel?",
-      name: "user",
       required: true,
       type: ApplicationCommandOptionType.User,
     })
     user: User | undefined,
+
+    @SlashOption({
+      name: "continuous",
+      description: "Want to be notified continously?",
+      required: true,
+      type: ApplicationCommandOptionType.String,
+    })
+    condition: string,
+
     interaction: CommandInteraction
   ): Promise<void> {
     //check if the user pair is already in the atMeListeners array
@@ -33,9 +42,10 @@ export class AtMe {
       //reply for confirmation
       await interaction.reply(`Now notifying ${user}`);
       //and add a listener for when the user joins the vc
-      user!.client.once(
-        "voiceStateUpdate",
-        (oldState, newState) => {
+      //check if the user wants to be notified continously or once-only
+      if (condition === "yes") {
+        //if they want to be notified continously, add a listener for when the user joins the vc
+        user!.client.on("voiceStateUpdate", (oldState, newState) => {
           if (
             oldState.channel === null &&
             newState.channel !== null &&
@@ -48,8 +58,24 @@ export class AtMe {
               }`
             );
           }
-        } //check if the user joined a vc
-      );
+        });
+      } else {
+        //if they want to be notified once-only, add a listener for when the user joins the vc
+        user!.client.once("voiceStateUpdate", (oldState, newState) => {
+          if (
+            oldState.channel === null &&
+            newState.channel !== null &&
+            newState.member!.id === user!.id
+          ) {
+            //if they did, ping the user
+            interaction.followUp(
+              `Hey ${interaction.user}, ${user!.username} just joined ${
+                newState.channel
+              }`
+            );
+          }
+        });
+      }
     }
   }
 }

102ec7fa710965249a51e38ae7c10656e00c290b
Milopadma <Milopadma@yahoo.com>
2022-10-09 15:11:12 +0800

    progress on adding notify

--- src/commands/atme.ts
+++ src/commands/atme.ts
@@ -1,6 +1,9 @@
 import { ApplicationCommandOptionType, CommandInteraction } from "discord.js";
 import { Discord, Slash, SlashOption } from "discordx";
-import type { GuildMember, User } from "discord.js";
+import type { User } from "discord.js";
+
+//define listeners array
+const atMeListenersPair: [User, User][] = [];
 
 @Discord()
 export class AtMe {
@@ -13,19 +16,38 @@ export class AtMe {
       required: true,
       type: ApplicationCommandOptionType.User,
     })
-    user: User | GuildMember | undefined,
+    user: User | undefined,
     interaction: CommandInteraction
   ): Promise<void> {
-    //reply for confirmation
-    //remove the @ to avoid spamming the user
-    const userName = user?.toString().replace("@", "");
-    await interaction.reply(`You will be notified when ${userName} joins a vc`);
-    //check when the user joins a vc
+    //check if the userpair already exists
+    const userPairExists = atMeListenersPair.some(
+      (userPair) => userPair[0] === interaction.user && userPair[1] === user
+    );
+    //if the userpair doesn't exist, add it to the array
+    if (!userPairExists) {
+      console.log("adding userpair" + userPairExists);
+      atMeListenersPair.push([interaction.user, user!]);
+      //reply for confirmation
+      await interaction.reply(
+        `You will be notified when ${user} joins a voice channel`
+      );
+    } else {
+      //if the userpair already exists, reply with an error message
+      await interaction.reply(
+        `You are already being notified when ${user} joins a voice channel`
+      );
+    }
+
+    //create a new listener for the user pair, when the user joins a vc, the interaction user will be notified
     interaction.client.on("voiceStateUpdate", (oldState, newState) => {
-      if (newState.id === user?.id && !oldState.channel && newState.channel) {
-        //notify the message sender when the user joins a vc
+      if (
+        newState.member?.user === user &&
+        newState.channel &&
+        !oldState.channel
+      ) {
+        console.log("user joined vc");
         interaction.followUp(
-          `Hey ${interaction.user.username}, ${user} is in a voice channel now!`
+          `Hey ${interaction.user}, ${user} just joined ${newState.channel}`
         );
       }
     });

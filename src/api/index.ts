ecadf08c0992db3c677a071b73c20a4f7ab4b7cc
mylo <48256865+Milopadma@users.noreply.github.com>
2022-10-16 20:24:15 +0800

    api calls

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,26 @@
 //prisma api calls
 // Path: src/api/index.ts
 
+import { Get, Router } from "@discordx/koa";
+import type { Context } from "koa";
+
+import { bot } from "../main.js";
+
+//prisma lib import
+import { prisma } from "../libs/prisma";
+
+@Router()
+export class API {
+  @Get("/")
+  index(context: Context): void {
+    //prisma get
+    const guilds = prisma.guildsArray.findMany();
+    context.body = guilds;
+    console.log(guilds);
+  }
+
+  @Get()
+  guilds(context: Context): void {
+    context.body = `${bot.guilds.cache.map((g) => `${g.id}: ${g.name}\n`)}`;
+  }
+}
